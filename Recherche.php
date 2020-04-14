0fefc0f8dc9f86ce5f6f3d895152b59db4211dd8
CollotPaul <47554035+CollotPaul@users.noreply.github.com>
2020-04-14 23:29:17 +0200

    ajout de la création de page perso
    modification de la feuilles de style et de la mise en page de la page accueil , recherche et objet perso
    ajout de function dans le js et simplification des différentes pages

--- Recherche.php
+++ Recherche.php
@@ -1,89 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Catégories</title>
+	<title><?php 
+    $categorie=$_GET['categorie'];
+    $type_achat=$_GET['type_achat'];
+    if($categorie!='null')
+      echo $categorie;
+    if($type_achat!='null')
+      echo $type_achat;
+  ?>
+  </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>  
-  <style type="text/css">
-  	.item:hover
-  	{
-  		opacity: 0.8;
-  	}
-  </style>  
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+  <script type="text/javascript" src="check.js">
+  function lancement_page(categorie,type_achat,ID)
+  {
+    window.open("page_objet.php?categorie="+categorie+"&type_achat="+type_achat+"&id="+ID,"_self");
+  } 
+  </script>
+ 
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="accueil.html">ECE Ebay</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Mettre en vente<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Bon Plan</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Mon Panier</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Connexion</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Mon Compte
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Mon Compte</a>
-            <a class="dropdown-item" href="#">Récemment regardé</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Mes Commandes</a>
-          </div>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav>
+  <?php include 'navbar.html';?>
+  <div class="container-fluid">
+  <?php
+    $categorie=null;
+    $type_achat=null;
+    $categorie=$_GET['categorie'];
+    $type_achat=$_GET['type_achat'];
+    $database="ebay_ece";
+    $db_handle = mysqli_connect('localhost','root','');
+    $db_found = mysqli_select_db($db_handle,$database);
+    if($db_found)
+    { 
+        $sql = "SELECT * from item where Categorie='".$categorie."' OR ".$type_achat."='1';";
+      $result = mysqli_query($db_handle, $sql);
+      while($data = mysqli_fetch_assoc($result))//==> nul s'il n'y a plus de ligne dans le tableau
+      {
+        echo "<div class='row' onclick=lancement_page('".$categorie."','".$type_achat."','".$data['ID']."')>";
+        echo "<div class='col-sm-6 item'>";
+        echo "<p><strong>Nom: </strong>" .$data['Nom']. "<br>";
+        echo "<strong>Prix: </strong>" .$data['Prix']. " euros<br>";
+        echo "<strong>Description: </strong><br>" .utf8_encode($data['Description']). "</p><br>";
+        echo "</div>";
+        echo "<div class='col-sm-6 item_image'>";
+        echo "<img src='https://www.citationbonheur.fr/wp-content/uploads/2018/09/L_influence_du_paysage_sur_le_bonheur.jpg' height='100%'>";
+        echo "</div>";
+        echo "</div>";
+      }
+    }
+    else
+    {
+      echo "database not found";
+    }
+    mysqli_close($db_handle);
+?>
+ </div>
  </body>
  </html>
 
-
-<?php
-$categorie=null;
-$type_achat=null;
-$categorie=$_GET['categorie'];
-$type_achat=$_GET['type_achat'];
-$database="ebay_ece";
-$db_handle = mysqli_connect('localhost','root','');
-$db_found = mysqli_select_db($db_handle,$database);
-if($db_found)
-{	
-  echo "<div class='container-fluid' style='padding:10px;'>";
-	  $sql = "SELECT * from item where Categorie='".$categorie."' OR ".$type_achat."='1';";
-	$result = mysqli_query($db_handle, $sql);
-	while($data = mysqli_fetch_assoc($result))//==> nul s'il n'y a plus de ligne dans le tableau
-	{
-		echo "<div class='row' style='padding:2px;'><div class='col-sm-6 item' style='border:solid 1px black; border-radius:20px; height:150px; background-color:pink;'>";
-		echo "Nom: " .$data['Nom']. "<br>";
-		echo "Prix: " .$data['Prix']. "euros<br>";
-		echo "Description: " .utf8_encode($data['Description']). "euros<br>";
-		echo "</div></div>";
-	}
-	echo "</div>";
-}
-else
-{
-	echo "database not found";
-}
-mysqli_close($db_handle);
-?>
+<!--onclick=lancement_page('".$categorie."','".$type_achat."','".$data['ID']."')-->
---
+++ page_objet.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Catégories</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>  
+</head>
+<body>
+<?php include 'navbar.html';?>
+<?php
+$categorie=$_GET['categorie'];
+$type_achat=$_GET['type_achat'];
+$id=$_GET['id'];
+
+$database="ebay_ece";
+$db_handle = mysqli_connect('localhost','root','');
+$db_found = mysqli_select_db($db_handle,$database);
+if($db_found)
+{ 
+  echo "<div class='container-fluid' style='padding:10px;'>";
+    $sql = "SELECT * from item where (Categorie='".$categorie."' OR ".$type_achat."='1') AND ID='".$id."';";
+  $result = mysqli_query($db_handle, $sql);
+  while($data = mysqli_fetch_assoc($result))//==> nul s'il n'y a plus de ligne dans le tableau
+  {
+    echo "<center><h1 style='border:1px grey double; border-radius:10px; '>" .$data['Nom']. "</h1></center><br>";
+    echo "<div class='container-fluid'><div class='row'><div class='col-sm-6'>";
+    echo "<p style='font-size: 30px;'><strong>Description:</strong><br> " .utf8_encode($data['Description']). "</p><br>";
+    echo "<h2>Prix: " .$data['Prix']. " euros</h2><br>";
+     if($data['vente_par_enchere']=='1')
+    {
+      echo "<div class='col-sm-4'><input class='form-control' type='number' value='".$data['Prix']."'>Euros<br><input class='btn btn-primary' style='font-size: 25px' type='button' value='Encherir'></div>";
+    }
+    if($data['vente_immediat']=='1')
+    {
+      echo "<div class='col-sm-4'><input class='btn btn-primary' style='font-size: 25px' type='button' value='Achat immédiat'></div>";
+    }
+    if($data['vente_par_meilleure_offre']=='1')
+    {
+      echo "<div class='col-sm-4'><input class='btn btn-primary' style='font-size: 25px' type='button' value='Achat par meilleure offre'></div>";
+    }
+    echo "</div>";
+    echo "<div class='col-sm-6'>";
+    echo "<img src='https://www.orseu-concours.com/181-318-thickbox/test-epso-de-raisonnement-numerique-3-fr.jpg'>";
+    echo "</div></div><div class='row'>";
+    echo "</div></div></div>";
+  }
+  echo "</div>";
+}
+else
+{
+  echo "database not found";
+}
+mysqli_close($db_handle);
+?>
+ </body>
+ </html>
