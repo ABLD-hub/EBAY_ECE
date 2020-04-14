199681997c2477c846abba8559b171e18bf5a1d3
CallMeMad <47555402+CallMeMad@users.noreply.github.com>
2020-04-14 10:47:18 +0200

    Add files via upload

---
+++ EBAY_ECE/check.js
@@ -0,0 +1,14 @@
+function cacher(id,ID)
+{
+	//document.write("Jean Michel1");
+	if(ID=="")
+	{
+		document.getElementById(id).style.visibility ="hidden";
+	}
+	else
+    {
+      document.getElementById(id).style.visibility="visible";
+    }
+    //return true;
+	//}
+}
